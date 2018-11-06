c40ea7b425ab4a686a9ff7cb8f80b454d6f0ec74
Amillz70 <adammiller7@yahoo.com>
2018-11-06 11:58:19 -0500

    Remove response from create curl script and uncomment out owner in survey.js
    Collabs- Adam and Vince

--- scripts/surveys/create.sh
+++ scripts/surveys/create.sh
@@ -10,8 +10,7 @@ curl "${API}${URL_PATH}" \
   --header "Authorization: Bearer ${TOKEN}" \
   --data '{
     "survey": {
-      "question": "'"${Q}"'",
-      "responses": [{"answer": "yes"}]
+      "question": "'"${Q}"'"
     }
   }'
 
