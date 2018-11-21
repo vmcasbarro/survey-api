571360e5f116ed9270e826e4c5915d9214a8b641
vmcasbarro <vincenzo.casbarro@gmail.com>
2018-11-21 17:04:45 -0500

    update db name and npm package files

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'best-survey-api'
+const mongooseBaseName = 'survey-api'
 
 // create the mongodb uri for development and test
 const database = {
